c67754db7b0cf388d7f46f8b77878ac205c92204
Ludovic Perrine <lpe@f4-group.com>
2015-02-27 18:12:35 +0100

    Fixed test

--- test/conversions.js
+++ test/conversions.js
@@ -1,10 +1,11 @@
-var should = require('should');
+/* global describe, it */
+require('should');
 var mercatorProjection = require("../mercatorProjection");
 
 describe('dateParser', function () {
     var start = {
-        lon: "2.3521334",
-        lat: "48.8565056",
+        lon: 2.3521334,
+        lat: 48.8565056,
     };
     var expected = {
         lon: 261838.2923294031,
@@ -16,17 +17,3 @@ describe('dateParser', function () {
         result.lat.should.be.approximately(expected.lat, 0.0001);
     });
 });
-
-
-function testDate(text, expectedStartDate, expectedEndDate) {
-    describe(text, function () {
-        var startDate = dateParser.parseStartDate(text);
-        var endDate = dateParser.parseEndDate(text);
-        it('startDate', function () {
-            startDate.should.be.eql(expectedStartDate);
-        });
-        it('endDate', function () {
-            endDate.should.be.eql(expectedEndDate);
-        });
-    });
-}
